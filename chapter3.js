a462ca03e98d9271ece250df33ed33327d5a812a
Ankit Shakya <ankit.laksoo11@gmail.com>
2025-07-01 16:17:38 +0545

    operators

---
+++ chapter3.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 10;
+console.log(a +b);
+
+let x = "david";
+let y = "loru";
+console.log(x + y);
+
+
+
+
+console.log(x + " " + "has 3 apples and "+ b + " oranges")
+//console.log("x" + 'has' +'a' 'apples' ,and + 'b' ,oranges);
+
+
+
+
+//"david" has 3 apples and "10" oranges
