4fcc521fe12d168b43ab6c433de474b86aa2916e
Ankit Shakya <ankit.laksoo11@gmail.com>
2025-07-04 23:21:37 +0545

    For Loops

---
+++ chapter5.js
@@ -0,0 +1,19 @@
+// for loops 
+
+for(let i = 0; i < 5; i++){
+    console.log('in loop', i);
+}
+
+console.log('loop finished');
+
+
+const nam = ['Frank', 'Colt', 'Piper'];
+
+for(let i =0; i<nam.length; i++){
+    
+    console.log(nam[i]);
+
+    let html = `<div>${nam[i]}</div>`;
+    console.log(html);
+
+}
